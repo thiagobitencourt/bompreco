f1d3f248ac975fd534bfd420c74ccd1c6f134383
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2015-09-01 00:03:04 -0300

    Algumas alteração em bannersController

--- tvWeb/js/controllers.js
+++ tvWeb/js/controllers.js
@@ -46,57 +46,49 @@ function($scope, $rootScope, $timeout, $location, sessao, sessaoService, produto
 	}
 })
 
-.controller("banersController", function($scope, $location, $timeout, sessao){
+.controller("banersController", function($scope, $location, $timeout, sessao, produtosService){
 
 	var sessoes = sessao.data;
 
-	$scope.produtos = sessoes.produtos;
-	console.log(sessoes.produtos);
+    var defineValor = function(produtos){
+	    var weekDay = new Array(7);
+	    weekDay[1] = "segunda-feira";
+	    weekDay[2] = "terça-feira"; 
+	    weekDay[3] = "quarta-feira";
+		weekDay[4] = "quinta-feira";
+		weekDay[5] = "sexta-feira";
+		weekDay[6] = "sábado";
+		weekDay[7] = "domingo";
 
-	// var showFunction = function(produtos){
-	// 	var pro = produtos.shift();
+    	var d = weekDay[new Date().getDay()];
 
-	// 	console.log(pro.produto);
+    	produtos.forEach(function(produto){
+	    	if(produto.valorEspecial && produto.valorEspecial.length > 0){
+	    		var dias = produto.valorEspecial;
 
-	// 	categoriasService.getCategoria(pro._id).success(function(data){
-	// 		$scope.currentCategoria = data;
+	    		dias.forEach(function(dia){
+	    			if(dia[d]){
+	    				produto.valorHoje = dia[d];
+	    			}else if(!produto.valorHoje){
+	    				produto.valorHoje = produto.valorPadrao;
+	    			}
+	    		});
+	    	}else{
+	    		produto.valorHoje = produto.valorPadrao;
+	    	}
+    	});
+    }
 
-	// 		console.log($scope.currentCategoria.categoria);
-			
-	// 		produtosService.getProdutos($scope.currentCategoria._id).success(function(data){
-	// 			$scope.produtos = data;
-	// 		});
-	// 	});
+    var loadProdutos = function(sessao){
+		produtosService.getProdutosBaner(sessao._id).success(function(data){
+			$scope.produtos = data;
+    		defineValor($scope.produtos);
+		});
+    }
 
-	// 	$timeout(function(){
-	// 		$location.path('/baners');
-	// 	}, pro.tempo * 1000);
-	// }
-
-	// if($rootScope.categorias && $rootScope.categorias.length > 0){
-		
-	// 	showFunction($rootScope.categorias);
-
-	// }else{
-
-	// 	if($rootScope.categorias){
-	// 		/*
-	// 		Se houver a variavel rootScope.categorias, significa que não é a primeira execução. 
-	// 		Então recarrega a sessão para pegar alguma possível atualização.
-	// 		*/
-	// 		sessaoService.getSessao(sessoes._id).success(function(data){
-	// 			sessoes = data;
-	// 		});
-	// 	}
-		
-	// 	$rootScope.categorias = sessoes.categorias;
-	// 	showFunction($rootScope.categorias);
-	// }
+    loadProdutos(sessoes);
 
 	$timeout(function(){
-
 		$location.path('/tabela');
-
 	}, 5000);
-
 });

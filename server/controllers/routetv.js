2850ba215c5119a4eefb206cc388787828aa99d2
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2015-11-11 11:32:28 -0200

    Permite abrir apenas a sessão padrão

--- server/controllers/routetv.js
+++ server/controllers/routetv.js
@@ -1,210 +1,14 @@
 var express = require('express');
-
 var Categorias = require('../models/categorias');
 var Produtos = require('../models/produtos');
 var Sessao = require('../models/sessao');
 
 var RouterTv = function(){
-
 	router = express.Router();
-
 	setRouteSessoes();
 	return router;
-/*
-	var buscaSessaoPadrao = function(done){
-		Sessao.findOne({padrao: true}, function(err, sessao){
-			if(err) return done(err, null);
-			done(null, sessao);
-		});
-	}
-
-	router.get('/sessao', function(req, res){
-
-		buscaSessaoPadrao(function(err, sessao){
-			if(err){
-				console.error("buscaSessaoPadrao Error: " + err.message);
-				return res.status(500).send({message: "500: Erro ao carregar sessão padrão"});
-			}
-
-			if(sessao){
-				return res.status(200).send(sessao);
-			}else{
-				var message = "Sessão padrão não encontrada";
-				console.log(message);
-				return res.status(400).send(message);
-			}
-		});
-	});
-
-	router.get('/sessao/:sessao', function(req, res){
-
-		//Redireciona qualquer requisição para a sessão padrão.
-		return res.redirect("/tv/sessao");
-
-		var nome = req.params.sessao;
-		if(!nome){
-			return res.redirect("/tv/sessao");
-		}
-
-		console.log("Nome da sessão: " + nome);
-
-		Sessao.findOne({nome: nome}, function(err, sessao){
-			if(err){
-				console.error("Sessao.findOne Error: " + err.message);
-				return res.status(500).send({message: "500: Erro ao carregar sessão"});
-			}
-
-			if(sessao){
-				res.status(200).send(sessao);
-			}
-		});
-	});
-
-	router.get('/sessao/hash/:id', function(req, res){
-
-		var id = req.params.id;
-
-		if(!id){
-			console.error("Error: ID de sessão não informado");
-			return res.status(500).send({message: "400: ID de sessão não informado"});
-		}
-
-		Sessao.findOne({_id: id}, function(err, sessao){
-			if(err){
-				console.error("Sessao.findOne Error: " + err.message);
-				return res.status(500).send({message: "500: Erro ao carregar sessão"});
-			}
-
-			if(sessao){
-				res.status(200).send(sessao.hash);
-			}
-		});
-	});
-
-	router.get('images/produtos/:name', function(req, res){
-
-		console.log("Getting Images");
-
-		var imgName = req.params.name;
-		res.sendfile('web/private/images/produtos/');
-		console.log(dir);
-		res.sendfile(dir + imgName);
-
-	});
-*/
-	// setRouteCategorias();
-	// setRouteProdutos();
-	// setRouteSessoes();
-
-	// return router;
 }
-/*
-var setRouteCategorias = function(){
-	var expressRouteSimple = "/categorias";
-	var expressRouteId =  expressRouteSimple + "/:id";
 
-	//Busca todas as categorias cadastradas
-	router.get(expressRouteSimple, function(req, res){
-
-		Categorias.find({}, function(err, categorias){
-			if(err){
-				console.error("Categorias.find Error: " + err.message);
-				return res.status(500).send({message: "500: Erro ao carregar categorias"});
-			}
-
-			res.status(200).send(categorias);
-		});
-	});
-
-		//Busca uma categoria por ID
-	router.get(expressRouteId, function(req, res){
-
-		var idCategoria = req.params.id;
-		if(!idCategoria){
-			console.warn("Categoria não informada");
-			return res.status(400).send({message: "Categoria não informada"});
-		}
-
-		Categorias.findById(idCategoria, function(err, categoria){
-			if(err){
-				console.error("Categorias.findById Error: " + err.message);
-				return res.status(500).send({message: "500: Erro ao carregar categoria"});
-			}
-
-			//Se não encontrar nenhuma categoria, retorna 404
-			if(!categoria){
-				console.error("Nenhuma categoria encontrada");
-				return res.status(404).send({message:"Nenhuma categoria encontrada"});
-			}
-
-			res.status(200).send(categoria);
-		});
-	});
-};
-
-var setRouteProdutos = function(){
-	var expressRouteSimple = "/produtos";
-	var expressRouteId =  expressRouteSimple + "/:id";
-
-	//Todos os produtos de uma categoria
-	router.get(expressRouteSimple + '/categoria/:id', function(req, res){
-
-		var categoriaId = req.params.id;
-		if(!categoriaId){
-			console.warn("Id de categoria inválido: " + categoriaId);
-			return res.status(400).send({message: "Id de categoria inválido"});
-		}
-
-		Produtos.find({categoria: categoriaId}, function(err, produtos){
-			if(err){
-				console.error("Produtos.find Error: " + err.message);
-				return res.status(500).send({message: "500: Erro ao carregar produtos da categora"});
-			}
-
-			res.status(200).send(produtos);
-		});
-	});
-
-	router.get(expressRouteSimple + '/sessao/:id', function(req, res){
-
-		var sessaoId = req.params.id;
-		if(!sessaoId){
-			console.warn("Id de sessão inválido: " + sessaoId);
-			return res.status(400).send({message: "Id de sessão inválido"});
-		}
-
-		Sessao.findOne({_id: sessaoId}, function(err, sessao){
-			if(err){
-				console.error("Sessao.findOne Error: " + err.message);
-				return res.status(500).send({message: "500: Erro ao carregar sessão"});
-			}
-
-			//Se não encontrar nenhuma sessao, retorna 404
-			if(!sessao){
-				console.error("Sessão não encontrada");
-				return res.status(404).send({message:"Sessão não encontrada"});
-			}
-
-			var produtosSes = [];
-			sessao.produtos.forEach(function(pr){
-				produtosSes.push(pr.produto);
-			});
-
-			Produtos.find({ _id: { "$in" : produtosSes} }, function(err, produtos){
-				if(err){
-					console.error("Produtos.find Error: " + err.message);
-					return res.status(500).send({message: "500: Erro ao carregar produtos da sessão"});
-				}
-
-				res.status(200).send(produtos);
-			});
-		});
-	});
-};
-*/
-
-
-// Use only this.
 var findProdutosByCategoria = function(categoria, callback){
 	var errH = function(err){
 		console.error("Produtos.find Error: " + err.message);
@@ -285,6 +89,7 @@ var setRouteSessoes = function(){
 
 	var handle = function(req, res){
 		var sesName = req.params.name;
+		sesName = null; //Remover para permitir carregar outras sessões, além da sessão padrão.
 		getSessao(sesName, function(err, sessao){
 			if(err){
 				console.error('Erro ao carregar sessão: ' + err);
